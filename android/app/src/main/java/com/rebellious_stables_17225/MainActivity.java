1ccdb207d2e5e84ee5da5673868af8addab5e4a9
Crowdbotics <team@crowdbotics.com>
2020-05-21 05:42:57 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/rebellious_stables_17225/MainActivity.java
@@ -0,0 +1,15 @@
+package com.rebellious_stables_17225;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "rebellious_stables_17225";
+    }
+}
